7f017373199f434521f53df6ec3f07b881713b5d
BirdBirdLee <1606251360@qq.com>
2020-06-16 01:43:12 +0800

    修复部署错误 是因为打包结束windows测试时未指定编码

--- src/main/java/cn/tcualhp/kbqa_tech_web/initialization/BuildCache.java
+++ src/main/java/cn/tcualhp/kbqa_tech_web/initialization/BuildCache.java
@@ -43,6 +43,9 @@ public class BuildCache {
     private static String graphPath = "data/graph/Neo4jTinyG.txt";
     private Driver driver;
 
+    // 所有的文件读取信息，用来测试部署的时候读取情况
+    public static Map<String, Object> allFilesInfo = new HashMap<String, Object>() ;
+
     // 将英文转化为文字的字典
     public static Map<String, String> aimExplanation = new HashMap<String, String>(){{
         put("aimWord_field", "领域");
@@ -61,6 +64,8 @@ public class BuildCache {
         String[] words = readtxtFile(entityDirPath, entityWordMap);
         ACTree acTree = new ACTree(words);
         acFilterEntity = new ACFilter(acTree);
+        // 测试
+        allFilesInfo.put("entityWords", entityWordMap);
     }
 
     /**
@@ -72,6 +77,8 @@ public class BuildCache {
         String[] words = readtxtFile(aimDirPath, aimWordMap);
         ACTree acTree = new ACTree(words);
         acFilterAim = new ACFilter(acTree);
+        // 测试
+        allFilesInfo.put("aimWords", aimWordMap);
     }
 
     /**
@@ -171,6 +178,7 @@ public class BuildCache {
                 BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
                 while ((word = br.readLine()) != null){
                     wordsList.add(word);
+//                    System.out.println("word:" + word);
                 }
                 String filename = resource.getFilename();
                 String wordsName = resource.getFilename().substring(0, resource.getFilename().length()-4);
